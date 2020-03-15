eb847bfaebd2cf3d173afa2ce507a234b74adefa
trigmag <61503947+trigmag@users.noreply.github.com>
2020-03-15 18:43:26 +0100

    Add files via upload

---
+++ js/scripts.js
@@ -0,0 +1,15 @@
+$('#yourelement').ready(function() {
+    $('.pop-up').click(function(){
+        var src = $(this).attr('src');
+        $('<div>').css({
+            background: 'RGBA(0,0,0,.5) url('+src+') no-repeat center',
+            width:'100%', height:'100%',
+            position:'fixed',
+            zIndex:'10000',
+            top:'0', left:'0',
+            cursor: 'zoom-out'
+        }).click(function(){
+            $(this).remove();
+        }).appendTo('body');
+    });
+});
